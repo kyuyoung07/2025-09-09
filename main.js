814304caf2782e4b005d3a8932c838707f615fe5
Kyuyoung <kyuyoung07@gmail.com>
2025-09-09 15:02:59 +0900

    second-commit

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 //+버튼을 클릭하면, 할일이 추가된다.
 //delete버튼 누르면 할일이 삭제된다.
 //check버튼 누르면 할일이 끝나면서 밑줄이 간다.
+//1. check버튼을 클릭하는 순간 true->false
+//2. true이면 끝난걸로 간주하고 밑줄 보여주기
+//3. false이면 안끝ㄴ나걸로 간주하고 그대로
+
 //진행중 끝남 탭을 누르면, 언더바가 이동한다.
 //끝남탭은 끝난 아이템만, 진행중탭은 진행중인 아이템만.
 //전체탭을 누르면 다시 전체아이템으로 돌아옴.
@@ -13,8 +17,12 @@ let taskList=[];
 addButton.addEventListener("click",addTask);
 
 function addTask(){
-    let taskContent=taskInput.value;
-    taskList.push(taskContent);
+    let task={
+        id:randomIDGenerate(),
+        taskContent:taskInput.value,
+        isComplete:false
+    };
+    taskList.push(task);
     console.log(taskList);
     render();
 }
@@ -22,15 +30,45 @@ function addTask(){
 function render(){
     let resultHTML='';
     for(let i=0;i<taskList.length;i++){
-        resultHTML+=`
+        if(taskList[i].isComplete==true){
+            resultHTML+=`
         <div class="task">
-            <div>${taskList[i]}</div>
+            <div class="task-done">${taskList[i].taskContent}</div>
             <div>
-                <button id="check-button">Check</button>
-                <button id="delete-button">Delete</button>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask">Delete</button>
             </div>
         </div>`
+        }else{
+            resultHTML+=`
+        <div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onclick="deleteTask()">Delete</button>
+            </div>
+        </div>`
+        }
     }
-
     document.getElementById("task-board").innerHTML=resultHTML;
+}
+
+function toggleComplete(id){
+
+    for(let i=0;i<taskList.length;i++){
+        if(taskList[i].id==id){
+            taskList[i].isComplete=!taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+
+function randomIDGenerate(){
+    return Math.random().toString(36).substr(2, 16);
+}
+
+function deleteTask(){
+    console.log("삭제하다")
 }
